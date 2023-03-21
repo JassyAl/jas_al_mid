7ff198a8a6b063180abd37867b8420ed6b1653c2
JassyAl <jasswalbert@gmail.com>
2023-03-20 19:48:07 -0500

    changed read_single

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -8,17 +8,17 @@ include_once '../../models/Author.php';
 
         }
         public function request_One($method, $id){
-            $auth_message = ["message" => 'author_id Not Found'];  
+            // $auth_message = ["message" => 'author_id Not Found'];  
             // if author exists
             if($id == ''){
-                echo json_encode($auth_message);
+                echo json_encode(["message" => 'author_id Not Found']);
                 exit;
             }
             // author query
             $res = $this->auth->read_single($id);
             // if authors return row count
             if($res->rowCount() == 0){
-                echo json_encode($auth_message);
+                echo json_encode(["message" => 'author_id Not Found']);
                 exit;
             }else {
                 // Create auth array
